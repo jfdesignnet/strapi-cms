9e2022fe5c6d2a26c62805229d7ce3c5af9d6db8
jfdesignnet <jfdesignnet@gmail.com>
2021-08-30 13:55:33 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '484625723193895d23c06ea8c546400c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c18a0190-74fe-469e-a902-aea78ec532cf'
+};
